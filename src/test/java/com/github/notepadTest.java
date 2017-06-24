c4528a5b07258bb6d6fe2c13f567e3a1a3ee7bc6
ramakellio <rkellio75@gmail.com>
2017-06-24 19:13:20 -0400

    Add files via upload

---
+++ src/test/java/com/github/notepadTest.java
@@ -0,0 +1,13 @@
+/* Modified the file 
+ * 
+ */
+
+public class notepadTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+}
