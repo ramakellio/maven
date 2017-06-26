a4d7851d837011ece7c95d0f3949ee7425dd39f1
ramakellio <ramakellio@gmail.com>
2017-06-25 21:34:07 -0400

    Added Assert function

--- src/test/java/com/github/AppTest.java
+++ src/test/java/com/github/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class AppTest 
 {
@@ -25,6 +25,6 @@ public class AppTest
     @Test
     public void testFalse()
     {
-        assertFalse("test failed", true );
+        fail("test failed" );
     }
 }
